84bd0a24371dbda9cd3f180c2c1bae1ed56c7b75
Sam Birch <mail@worldofsam.com>
2019-07-09 08:34:55 +1200

    Publishing can now be delayed at startup to allow readings time to stabilise

--- src/logger.py
+++ src/logger.py
@@ -34,28 +34,36 @@ class EnvLogger:
             self.connection_error = errors.get(rc, "unknown error")
 
 
+    def take_readings(self):
+        gas_data = gas.read_all()
+        readings = {
+            "proximity": ltr559.get_proximity(),
+            "lux": ltr559.get_lux(),
+            "temperature": self.bme280.get_temperature(),
+            "pressure": self.bme280.get_pressure(),
+            "humidity": self.bme280.get_humidity(),
+            "gas/oxidising": gas_data.oxidising,
+            "gas/reducing": gas_data.reducing,
+            "gas/nh3": gas_data.nh3,
+        }
+
+        if self.pms5003 is not None:
+            pm_data = self.pms5003.read()
+            readings["particulate/1.0"] = pm_data.pm_ug_per_m3(1.0)
+            readings["particulate/2.5"] = pm_data.pm_ug_per_m3(2.5)
+            readings["particulate/10.0"] = pm_data.pm_ug_per_m3(10)
+        
+        return readings
+
+
     def publish(self, topic, value):
         topic = self.prefix.strip("/") + "/" + topic
         self.client.publish(topic, str(value))
 
 
-    def update(self):
-        self.publish("proximity", ltr559.get_proximity())
-        self.publish("lux", ltr559.get_lux())
-
-        self.publish("temperature", self.bme280.get_temperature())
-        self.publish("pressure", self.bme280.get_pressure())
-        self.publish("humidity", self.bme280.get_humidity())
-
-        gas_data = gas.read_all()
-        self.publish("gas/oxidising", gas_data.oxidising)
-        self.publish("gas/reducing", gas_data.reducing)
-        self.publish("gas/nh3", gas_data.nh3)
-
-        if self.pms5003 is not None:
-            pm_data = self.pms5003.read()
-            self.publish("particulate/1.0", pm_data.pm_ug_per_m3(1.0))
-            self.publish("particulate/2.5", pm_data.pm_ug_per_m3(2.5))
-            self.publish("particulate/10.0", pm_data.pm_ug_per_m3(10))
-
+    def update(self, publish_readings=True):
+        readings = self.take_readings()
+        if publish_readings:
+            for (topic, value) in readings.items():
+                self.publish(topic, value)
         self.client.loop()
--- src/main.py
+++ src/main.py
@@ -12,6 +12,7 @@ def parse_args():
     ap.add_argument("--prefix", default="", help="the topic prefix to use when publishing readings, i.e. 'lounge/enviroplus'")
     ap.add_argument("--client-id", default="", help="the MQTT client identifier to use when connecting")
     ap.add_argument("--interval", type=int, default=5, help="the duration in seconds between updates")
+    ap.add_argument("--delay", type=int, default=15, help="the duration in seconds to allow the sensors to stabilise before starting to publish readings")
     ap.add_argument("--use-pms5003", action="store_true", help="if set, PM readings will be taken from the PMS5003 sensor")
     ap.add_argument("--help", action="help", help="print this help message and exit")
     return vars(ap.parse_args())
@@ -30,11 +31,12 @@ def main():
         use_pms5003=args["use_pms5003"],
     )
 
+    publish_start_time = time.time() + args["delay"]
     while True:
         if logger.connection_error is not None:
             sys.exit(f"Connecting to the MQTT server failed: {logger.connection_error}")
         
-        logger.update()
+        logger.update(publish_readings=time.time() > publish_start_time)
         time.sleep(args["interval"])
 
 
